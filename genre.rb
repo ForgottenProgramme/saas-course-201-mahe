047213db3837bc209358cd4003264aebb47041a8
ForgottenProgramme <forgotten.programme@gmail.com>
2021-01-17 15:05:34 +0530

    genre selection program

---
+++ genre.rb
@@ -0,0 +1,12 @@
+puts "What's your favourite genre? Fiction or Non-fiction?"
+genre = gets.chomp.downcase
+
+if genre=='fiction'
+    puts "Read Harry Potter"
+elsif genre=='non-fiction'
+    puts "Read 'The Theory of Computation' "
+else 
+    puts "Go play cricket, you bufoon"
+end
+
+#useless comment
