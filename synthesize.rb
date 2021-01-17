d3e7f53783a1313f35b07c3067f68e00f03144e1
ForgottenProgramme <forgotten.programme@gmail.com>
2021-01-17 21:56:18 +0530

    hash function using symbols

--- synthesize.rb
+++ synthesize.rb
@@ -40,4 +40,43 @@ puts "The books in my catalog are #{books.join(", ")}"
 library.each do |author, books|
     puts "#{author.capitalize} wrote the books #{books.join(', ')}"
 end
+puts "\n\n\n"
+#symbols
 
+a = :some
+b = :"multiple words"
+c = [:x, :y]
+library = {
+    :austen => ["a", "b"],
+    :someshit => ["c", "d"]
+}
+library[:rowling] = ["x"]
+#puts library
+
+#synthesize 2 
+
+#problem 1
+books = ["Design as Art", "Anathem", "Shogun"]
+authors = ["Bruno Munari", "Neal Stephenson", "James Clavell"]
+
+sym_hash = {}
+authors.map.with_index{ |name, i| sym_hash[name.partition(" ").first.downcase.to_sym]=[books[i]]}
+puts sym_hash
+puts "\n\n\n"
+#problem 2
+
+todos = [
+    ["Send invoice", "money"],
+    ["Clean room", "organize"],
+    ["Pay rent", "money"],
+    ["Arrange books", "organize"],
+    ["Pay taxes", "money"],
+    ["Buy groceries", "food"]
+  ]
+
+todo_hash = {}
+todos.map.with_index{ |todo, i| todo_hash[todo[1].to_sym] ||= []
+    todo_hash[todo[1].to_sym].push(todo[0])}
+todo_hash[:another] = []
+todo_hash.compact
+puts todo_hash
